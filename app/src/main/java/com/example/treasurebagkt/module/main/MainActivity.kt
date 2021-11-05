95ed9d0f31281b492585cf120a864617ce054865
ynov <yangjw@snxun.com>
2021-11-05 11:32:26 +0800

    Initial commit

---
+++ app/src/main/java/com/example/treasurebagkt/module/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.treasurebagkt.module.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.treasurebagkt.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
